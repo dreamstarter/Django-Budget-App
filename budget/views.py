ef6408eea595312e88580e5f5560416e17d4ae50
Bryan Anderson <dreamstarter@gmail.com>
2019-04-04 14:55:28 -0400

    updated models and detail view

--- budget/views.py
+++ budget/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 def project_list(request):
     return render(request, 'budget/project-list.html')
 
 def project_detail(request, project_slug):
-    # fetching the correct project
-    return render(request, 'budget/project-detail.html')
+    project = get_object_or_404(Project, slug=project_slug)
+    return render(request, 'budget/project-detail.html', {'project': project, 'expense_list': project.expenses.all()})
